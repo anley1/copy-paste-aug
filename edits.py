11fbd3836be39df4014ed0a9ca037f1f87e06593
Alexander Leye <anley1@student.monash.edu>
2021-09-28 13:10:26 +1000

    stable

---
+++ edits.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import json
+from copy_paste import CopyPaste
+from coco import CocoDetectionCP
+from visualize import display_instances
+import albumentations as A
+import random
+from matplotlib import pyplot as plt
+
+
+def edit_unique_anns():
+    with open("aug.json", 'r') as j_file:
+        data = json.load(j_file)
+
+    anno_len = len(data['annotations'])
+
+    count = 0
+    for ix in range(anno_len):
+        data['annotations'][ix]['id'] = count
+        count += 1
+
+    with open("new.json", 'w') as j_file:
+        json.dump(data, j_file)
+
+
+def edit_jpg_ext():
+    with open("aug.json", 'r') as j_file:
+        data = json.load(j_file)
+
+    img_len = len(data['images'])
+
+    for ix in range(img_len):
+        data['images'][ix]['file_name'] = data['images'][ix]['file_name'] + \
+                                          '.jpg'
+
+    with open("new.json", 'w') as j_file:
+        json.dump(data, j_file)
+
+
+if __name__ == "__main__":
+    edit_jpg_ext()
--- synthesize.py
+++ synthesize.py
@@ -1,35 +1,25 @@
-import cv2
-import numpy as np
-import json
 from copy_paste import CopyPaste
 from coco import CocoDetectionCP
-from visualize import display_instances
 import albumentations as A
-import random
-from matplotlib import pyplot as plt
 
 transformScene = A.Compose([
         #A.RandomScale(scale_limit=(-0.9, 1), p=1), #LargeScaleJitter from scale of 0.1 to 2
         A.PadIfNeeded(256, 256, border_mode=0), #pads with image in the center, not the top left like the paper
-        #A.Resize(800, 1333, always_apply=True, p=1),
         A.Resize(534, 889, always_apply=True, p=1),
-        #A.RandomCrop(534, 889),
-        #A.Resize(800, 1333, always_apply=True, p=1),
     ]
-    # bbox_params=A.BboxParams(format="coco", min_visibility=0.05)
 )
 
 
 transform = A.Compose([
         A.RandomScale(scale_limit=(-0.9, 1), p=1), #LargeScaleJitter from scale of 0.1 to 2
         A.PadIfNeeded(256, 256, border_mode=0), #pads with image in the center, not the top left like the paper
-        A.Flip(always_apply=True, p=0.5),
+        A.Flip(p=0.5),
         A.Resize(800, 1333, always_apply=True, p=1),
         #A.Solarize(always_apply=True, p=1.0, threshold=(128, 128)),
         A.RandomCrop(534, 889),
         #A.Resize(800, 1333, always_apply=True, p=1),
         # pct_objects is the percentage of objects to paste over
-        CopyPaste(blend=True, sigma=1, pct_objects_paste=1, p=1.)
+        CopyPaste(blend=True, sigma=1, pct_objects_paste=1, p=1., gray=True)
     ], bbox_params=A.BboxParams(format="coco")
 )
 
@@ -40,16 +30,19 @@ transform = A.Compose([
 #    transform
 #)
 data = CocoDetectionCP(
-    '../Swin-Transformer-Object-Detection/data/flooding_high_cropped',
-    # '../agilent-repos/mmdetection/data/bead_cropped_detection/images',
-    #'../Swin-Transformer-Object-Detection/data/bead_cropped_detection/traintype2lower.json',
-    '../Swin-Transformer-Object-Detection/data/beading_basler',
-    '../Swin-Transformer-Object-Detection/data/basler_bead_non_cropped.json',
+    '../Swin-Transformer-Object-Detection/data/flooding_high_cropped_gray',
+    '../Swin-Transformer-Object-Detection/data/bead_cropped_detection/Grayscale',
+    #'../Swin-Transformer-Object-Detection/data/beading_basler',
+    #'../Swin-Transformer-Object-Detection/data/basler_bead_non_cropped.json',
+    '../Swin-Transformer-Object-Detection/data/bead_cropped_detection/traintype2lower.json',
     transform,
-    transformScene
+    transformScene,
+    gray=True
 )
 
 # Synthesize a new dataset using all of the scene images, and the available
 # annotations from the objects images (randomly).
-data.synthesize()
+data.synthesize(pct=0.5,
+                out_dir="out_medium_gray/",
+                json_name="aug_medium_gray.json")
 
